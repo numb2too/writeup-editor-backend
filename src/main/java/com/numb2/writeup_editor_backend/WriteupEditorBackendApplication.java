45251eaf9cd8e65b66eb768bae4628d435954246
numb2too <milk518abc@gmail.com>
2025-11-16 19:03:33 +0800

    init

---
+++ src/main/java/com/numb2/writeup_editor_backend/WriteupEditorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.numb2.writeup_editor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriteupEditorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriteupEditorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/numb2/writeup_editor_backend/WriteupEditorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numb2.writeup_editor_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteupEditorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
